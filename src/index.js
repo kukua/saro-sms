d61e023010623c5b583e0b13eb35087d54c6329f
Acellam Guy <abiccel@yahoo.com>
2017-12-30 12:52:28 +0300

    Support sending sms for daily and four day

--- src/index.js
+++ src/index.js
@@ -14,7 +14,8 @@ require( "dotenv" ).config();
 const englishTrans = require( path.resolve( "locales/en" ) );
 const swahilliTrans = require( path.resolve( "locales/sw" ) );
 const sendersList = require( path.resolve( process.env.SENDERS_DB_PATH ) );
-const recipientsList = require( path.resolve( process.env.RECIPIENTS_DB_PATH ) );
+const recipientsDailyForcastList = require( path.resolve( process.env.RECIPIENTS_DAILY_DB_PATH ) );
+const recipientsFourDayCastList = require( path.resolve( process.env.RECIPIENTS_FOUR_DAY_DB_PATH ) );
 
 const twilio = new Twilio( process.env.TWILIO_SID, process.env.TWILIO_AUTH_TOKEN );
 const senderID = String( process.env.SENDER_ID || "" );
@@ -195,96 +196,107 @@ function getFourDayForecast( body, recipient ) {
     return text;
 }
 
-try {
-    const validNumber = /^\+[0-9]{11,13}$/;
+// eslint-disable-next-line no-unused-vars
+function sendDailyForecast() {
+    sendForeCast( recipientsDailyForcastList );
+}
+// eslint-disable-next-line no-unused-vars
+function sendFourDayForecast() {
+    sendForeCast( recipientsFourDayCastList );
+}
 
-    sendersList.forEach( ( number ) => {
-        if ( !number.match( validNumber ) ) {
-            return error( { number }, "Invalid sender Twilio number." );
-        }
-        return true;
-    } );
+function sendForeCast( recipientsList ) {
+    try {
+        const validNumber = /^\+[0-9]{11,13}$/;
 
-    const recipients = recipientsList.map( ( recipient, i ) => {
-        const newRecipient = recipient;
-        const twilioNumber = sendersList[ i % sendersList.length ];
-
-        // Do validation, Note: recipient.name is optional
-        if ( !recipient.location ) return error( { recipient }, "Missing recipient location." );
-        if ( !recipient.number ) return error( { recipient }, "Missing recipient number." );
-        if ( !recipient.latitude ) return error( { recipient }, "Missing recipient latitude." );
-        if ( !recipient.longitude ) return error( { recipient }, "Missing recipient longitude." );
-        if ( !recipient.number.match( validNumber ) ) return error( { recipient }, "Invalid recipient number." );
-        if ( !twilioNumber ) return error( { recipient }, "Invalid recipient Twilio number." );
-
-        newRecipient.name = ( recipient.name || "Unnamed" ).toUpperCase().trim();
-        newRecipient.language = ( recipient.language || "en" ).trim();
-        newRecipient.location = recipient.location.toUpperCase().trim();
-        newRecipient.twilioNumber = twilioNumber;
-        newRecipient.format = ( typeof recipient.format === "number" ? recipient.format : 1 );
-
-        return newRecipient;
-    } );
-
-    // console.log(senders, recipients); process.exit(1)
-
-    recipients.forEach( ( recipient, i ) => {
-        p.add( ( done ) => {
-            setTimeout( () => {
-                const url = `${ process.env.NAVIFEED_URL }${ getFTimes( recipient.format ) }&lat=${ recipient.latitude }&lon=${ recipient.longitude }`;
-
-                request( {
-                    method: "GET",
-                    url,
-                    headers: {
-                        Accept: "text/xml",
-                    },
-                }, ( err, res, body ) => {
-                    if ( err ) return error( err );
-
-                    try {
-                        log.debug( { type: "raw", recipient, body } );
-
-                        if ( recipient.format === 1 ) {
-                            const { morning, afternoon, evening } = measurementsForDate( today, body, recipient, done );
-
-                            const text = prefixWithLocation( recipient.location, [
-                                `${ today.format( "MMM D" ) }`,
-                                createTextLineFormat1( "Morn", morning ),
-                                createTextLineFormat1( "Aft", afternoon ),
-                                createTextLineFormat1( "Eve", evening ),
-                            ].join( "\n" ) );
-
-                            sendText( senderID, recipient.number, text, done );
-                        } else if ( recipient.format === 2 ) {
-                            const { night, morning, afternoon, evening } = measurementsForDate( tomorrow, body, recipient, done );
-
-                            const text = prefixWithLocation( recipient.location, [
-                                `${ tomorrow.format( "MMM D" ) }`,
-                                createTextLineFormat2( "Night", night ),
-                                createTextLineFormat2( "Morning", morning ),
-                                createTextLineFormat2( "Afternoon", afternoon ),
-                                createTextLineFormat2( "Evening", evening ),
-                            ].join( "\n" ) );
-
-                            sendText( recipient.twilioNumber, recipient.number, text, done );
-                        } else if ( recipient.format === 3 ) {
-                            const text = getFourDayForecast( body, recipient );
-
-                            sendText( recipient.twilioNumber, recipient.number, text, done );
-                        }
-                    } catch ( e ) {
-                        done( e );
-                    }
-                } );
-            }, i * sendInterval );
+        sendersList.forEach( ( number ) => {
+            if ( !number.match( validNumber ) ) {
+                return error( { number }, "Invalid sender Twilio number." );
+            }
+            return true;
         } );
-    } );
 
-    p.done( ( err ) => {
-        if ( err ) return error( err );
-        log.info( "Done." );
-    } );
-} catch ( err ) {
-    error( err );
+        const recipients = recipientsList.map( ( recipient, i ) => {
+            const newRecipient = recipient;
+            const twilioNumber = sendersList[ i % sendersList.length ];
+
+            // Do validation, Note: recipient.name is optional
+            if ( !recipient.location ) return error( { recipient }, "Missing recipient location." );
+            if ( !recipient.number ) return error( { recipient }, "Missing recipient number." );
+            if ( !recipient.latitude ) return error( { recipient }, "Missing recipient latitude." );
+            if ( !recipient.longitude ) return error( { recipient }, "Missing recipient longitude." );
+            if ( !recipient.number.match( validNumber ) ) return error( { recipient }, "Invalid recipient number." );
+            if ( !twilioNumber ) return error( { recipient }, "Invalid recipient Twilio number." );
+
+            newRecipient.name = ( recipient.name || "Unnamed" ).toUpperCase().trim();
+            newRecipient.language = ( recipient.language || "en" ).trim();
+            newRecipient.location = recipient.location.toUpperCase().trim();
+            newRecipient.twilioNumber = twilioNumber;
+            newRecipient.format = ( typeof recipient.format === "number" ? recipient.format : 1 );
+
+            return newRecipient;
+        } );
+
+        // console.log(senders, recipients); process.exit(1)
+
+        recipients.forEach( ( recipient, i ) => {
+            p.add( ( done ) => {
+                setTimeout( () => {
+                    const url = `${ process.env.NAVIFEED_URL }${ getFTimes( recipient.format ) }&lat=${ recipient.latitude }&lon=${ recipient.longitude }`;
+
+                    request( {
+                        method: "GET",
+                        url,
+                        headers: {
+                            Accept: "text/xml",
+                        },
+                    }, ( err, res, body ) => {
+                        if ( err ) return error( err );
+
+                        try {
+                            log.debug( { type: "raw", recipient, body } );
+
+                            if ( recipient.format === 1 ) {
+                                const { morning, afternoon, evening } = measurementsForDate( today, body, recipient, done );
+
+                                const text = prefixWithLocation( recipient.location, [
+                                    `${ today.format( "MMM D" ) }`,
+                                    createTextLineFormat1( "Morn", morning ),
+                                    createTextLineFormat1( "Aft", afternoon ),
+                                    createTextLineFormat1( "Eve", evening ),
+                                ].join( "\n" ) );
+
+                                sendText( senderID, recipient.number, text, done );
+                            } else if ( recipient.format === 2 ) {
+                                const { night, morning, afternoon, evening } = measurementsForDate( tomorrow, body, recipient, done );
+
+                                const text = prefixWithLocation( recipient.location, [
+                                    `${ tomorrow.format( "MMM D" ) }`,
+                                    createTextLineFormat2( "Night", night ),
+                                    createTextLineFormat2( "Morning", morning ),
+                                    createTextLineFormat2( "Afternoon", afternoon ),
+                                    createTextLineFormat2( "Evening", evening ),
+                                ].join( "\n" ) );
+
+                                sendText( recipient.twilioNumber, recipient.number, text, done );
+                            } else if ( recipient.format === 3 ) {
+                                const text = getFourDayForecast( body, recipient );
+
+                                sendText( recipient.twilioNumber, recipient.number, text, done );
+                            }
+                        } catch ( e ) {
+                            done( e );
+                        }
+                    } );
+                }, i * sendInterval );
+            } );
+        } );
+
+        p.done( ( err ) => {
+            if ( err ) return error( err );
+            log.info( "Done." );
+        } );
+    } catch ( err ) {
+        error( err );
+    }
 }
